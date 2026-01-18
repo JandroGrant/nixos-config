1647df69ed2ce613f0e6a0382f3330e95996b9f9
JandroGrant <jandrocottonball@gmail.com>
2026-01-18 00:57:16 -0800

    added rofi to programs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -30,7 +30,7 @@ in {
     ./programs/fish
     ./programs/hyprshot
     ./programs/rmpc
-    # ./programs/rofi
+    ./programs/rofi { inherit config; }
     ./programs/vesktop
     ./programs/yazi
     ./programs/zoxide
@@ -54,45 +54,45 @@ in {
       };
     };
 
-    rofi = {
-      enable = true;
-      font = "FantasqueSansM Nerd Font 12";
-      theme = let
-        mkLiteral = config.lib.formats.rasi.mkLiteral;
-      in {
-        "@import" = "${config.xdg.cacheHome}/wal/colors-rofi-dark.rasi";
-        "window" = {
-          anchor = mkLiteral "north";
-          location = mkLiteral "north";
-          width = mkLiteral "100%";
-          padding = mkLiteral "4px";
-          children = [
-            "horibox"
-          ];
-        };
-        "horibox" = {
-          orientation = mkLiteral "horizontal";
-          children = [
-            "prompt"
-            "entry"
-            "listview"
-          ];
-        };
-        "listview" = {
-          layout = mkLiteral "horizontal";
-          spacing = mkLiteral "5px";
-          lines = mkLiteral "100";
-        };
-        "entry" = {
-          expand = mkLiteral "false";
-          width = mkLiteral "10em";
-        };
-        "element-text, element-icon" = {
-          background-color = mkLiteral "inherit";
-          text-color = mkLiteral "inherit";
-        };
-      };
-    };
+    # rofi = {
+    #   enable = true;
+    #   font = "FantasqueSansM Nerd Font 12";
+    #   theme = let
+    #     mkLiteral = config.lib.formats.rasi.mkLiteral;
+    #   in {
+    #     "@import" = "${config.xdg.cacheHome}/wal/colors-rofi-dark.rasi";
+    #     "window" = {
+    #       anchor = mkLiteral "north";
+    #       location = mkLiteral "north";
+    #       width = mkLiteral "100%";
+    #       padding = mkLiteral "4px";
+    #       children = [
+    #         "horibox"
+    #       ];
+    #     };
+    #     "horibox" = {
+    #       orientation = mkLiteral "horizontal";
+    #       children = [
+    #         "prompt"
+    #         "entry"
+    #         "listview"
+    #       ];
+    #     };
+    #     "listview" = {
+    #       layout = mkLiteral "horizontal";
+    #       spacing = mkLiteral "5px";
+    #       lines = mkLiteral "100";
+    #     };
+    #     "entry" = {
+    #       expand = mkLiteral "false";
+    #       width = mkLiteral "10em";
+    #     };
+    #     "element-text, element-icon" = {
+    #       background-color = mkLiteral "inherit";
+    #       text-color = mkLiteral "inherit";
+    #     };
+    #   };
+    # };
 
     tmux = {
       enable = true;
---
+++ home-manager/programs/rofi/default.nix
@@ -0,0 +1,41 @@
+{ config, ... }: {
+  programs.rofi = {
+    enable = true;
+    font = "FantasqueSansM Nerd Font 12";
+    theme = let
+      mkLiteral = config.lib.formats.rasi.mkLiteral;
+    in {
+      "@import" = "${config.xdg.cacheHome}/wal/colors-rofi-dark.rasi";
+      "window" = {
+        anchor = mkLiteral "north";
+        location = mkLiteral "north";
+        width = mkLiteral "100%";
+        padding = mkLiteral "4px";
+        children = [
+          "horibox"
+        ];
+      };
+      "horibox" = {
+        orientation = mkLiteral "horizontal";
+        children = [
+          "prompt"
+          "entry"
+          "listview"
+        ];
+      };
+      "listview" = {
+        layout = mkLiteral "horizontal";
+        spacing = mkLiteral "5px";
+        lines = mkLiteral "100";
+      };
+      "entry" = {
+        expand = mkLiteral "false";
+        width = mkLiteral "10em";
+      };
+      "element-text, element-icon" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+    };
+  };
+}
