e4534dbdda897c414817d41e8eab9bc16a7f195f
JandroGrant <jandrocottonball@gmail.com>
2026-01-18 00:20:32 -0800

    testing

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -30,37 +30,6 @@ in {
 
   programs = {
 
-    fastfetch = {
-      enable = true;
-      settings = {
-        logo = {
-          type = "auto";
-          source = "/home/jandro/images/hello-abq.png";
-          width = 18;
-          height = 13;
-          padding = {
-            top = 1;
-            right = 2;
-          };
-        };
-        modules = [
-          "title"
-          "separator"
-          "os"
-          "kernel"
-          "uptime"
-          "packages"
-          "shell"
-          "wm"
-          "theme"
-          "terminal"
-          "terminalfont"
-          "memory"
-          "colors"
-        ];
-      };
-    };
-
     fish = {
       enable = true;
       shellAliases = {
