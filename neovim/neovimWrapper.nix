57dfce1832f9c83d64a5ec5c1988c52c7d13d31e
JandroGrant <jandrocottonball@gmail.com>
2026-01-19 14:38:56 -0800

    added neovim config settings

--- neovim/neovimWrapper.nix
+++ neovim/neovimWrapper.nix
@@ -4,5 +4,27 @@ in
   pkgs.wrapNeovim pkgs.neovim {
     configure = {
       packages.all.start = plugins;
+      customLuaRC = ''
+        vim.g.mapleader = " "
+        vim.g.maplocalleader = "\\"
+
+        -- basic settings
+        vim.opt.number = true
+	      vim.opt.relativenumber = true
+	      vim.opt.cursorline = true
+	      vim.opt.scrolloff = 10
+	      vim.opt.sidescrolloff = 8
+	      vim.opt.wrap = false
+	      vim.opt.cmdheight = 1
+	      vim.opt.spelllang = { "en", "es" }
+
+	      -- tabbing and indentation
+	      vim.opt.tabstop = 2
+	      vim.opt.shiftwidth = 2
+	      vim.opt.softtabstop = 2
+	      vim.opt.expandtab = true
+	      vim.opt.smartindent = true
+	      vim.opt.autoindent = true
+      '';
     };
   }
