f24b7d3b7f8d83f8a5dfd432f239f730e989ca1a
JandroGrant <jandrocottonball@gmail.com>
2026-01-20 15:46:05 -0800

    fixed lualine lol

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -66,7 +66,7 @@
             options = {
               theme = "pywal",
               icons_enabled = true,
-              section_separators = { left = "", right = "" },
+              section_separators = { left = "", right = "" },
               components_separators = "|",
             }
           })
